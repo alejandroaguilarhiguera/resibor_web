116d0a1805bda5c98982f0b1e79ae6aec0124591
alejandro aguilar higuera <aaguilar@adhocti.com>
2021-08-02 21:55:45 -0600

    fix logo init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
           {' '}
