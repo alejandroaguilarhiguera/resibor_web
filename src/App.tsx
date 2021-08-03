dcf692fa98e74bc6a046023f2f6a43ef34dcd908
alejandro aguilar higuera <aaguilar@adhocti.com>
2021-08-02 21:54:39 -0600

    add prettierc

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import './App.css';
 
-const App: React.FC = () => {
+function App() {
   return (
-    <div>
-      <p>
-        Hola mundo
-      </p>
+    <div className="App">
+      <header className="App-header">
+        <img className="App-logo" alt="logo" />
+        <p>
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
